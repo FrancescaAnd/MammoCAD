3afc41f4c22ba3cda630f98121b6e92c86ef35a8
FrancescaAnd <103113486+FrancescaAnd@users.noreply.github.com>
2025-03-19 17:17:09 +0100

    Add files via upload

---
+++ mammograpy_cad/main_esrgan.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+from utils.split_dataset_esrgan import split_dataset_esrgan
+from utils.esrgan_data import generate_pairs
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', type=str, required=True, choices=['train', 'val', 'split','test'])
+    parser.add_argument('--data', type=str, default="clahe", help='dataset to use (basic augmented or augmented plus clahe')
+    args = parser.parse_args()
+
+
+    if args.mode == "train":
+        os.system("python train/esrgan_train.py")
+    elif args.mode == "val":
+        os.system("python eval/val_esrgan.py")
+    elif args.mode == "split":
+        if args.data == "clahe":
+            generate_pairs("data/processed/clahePNG/", "data/esrgan_data/LR", "data/esrgan_data/HR", scale=4)
+            split_dataset_esrgan(args.json_path, args.out_dir)
+        elif args.mode == "augmented":
+            generate_pairs("data/processed/augmentedPNG/", "data/esrgan_data/LR", "data/esrgan_data/HR", scale=4)
+            split_dataset_esrgan(args.json_path, args.out_dir)
+    elif args.mode == "test":
+        os.system("python eval/test_esrgan.py")
